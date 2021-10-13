c913c28e305201b9af9e2fe96241af21622cfc04
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-10-13 22:24:57 +0300

    Update accessoryController.js

---
+++ src/controllers/accessoryController.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+
+const accessoryService;
+
+let options = null;
+
+const isAuthenticated = (req, res, next) => {
+    const { user } = req;
+
+    if (!user) {
+        return res.redirect('/');
+    }
+
+    next();
+};
+
+const renderAddAccessoryPageHandler = (req, res) => {
+    const { isAuthenticated } = req;
+
+    options = {
+        isAuthenticated
+    };
+
+    res.render('accessories/create', options);
+};
+
+const addAccessoryHandler = async (req, res) => {
+    const { name, description, imageUrl } = req.body;
+    
+    try {
+        await accessoryService.create(name, description, imageUrl);
+        res.redirect('/');
+    } catch (err) {
+        // console.log(err.message);
+        res.status(500).send(err.message);
+    }
+};
+
+router.get('/create', isAuthenticated, renderAddAccessoryPageHandler);
+router.post('/create', isAuthenticated, addAccessoryHandler);
+
+module.exports = router;
