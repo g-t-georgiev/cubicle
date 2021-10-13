fe93f36d9288d126e08e1382050e009b7aa67df7
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-10-13 21:31:01 +0300

    Update cubeService.js

---
+++ src/services/cubeService.js
@@ -0,0 +1,70 @@
+const Cube = require('../models/Cube');
+const Accessory = require('../models/Accessory');
+
+const get = (id) => Cube.findById(id).populate('accessories').lean();
+
+const getAll = () => Cube.find({}).lean();
+
+const create = (name, description, imageUrl, difficultyLvl, creatorId) => Cube.create({ name, description, imageUrl, difficultyLvl, creatorId });
+
+const edit = (id, data) => Cube.findByIdAndUpdate(id, { ...data });
+
+const remove = (id) => Cube.findByIdAndDelete(id);
+
+const search = async function (searchText, from, to) {
+    searchText = searchText.trim().toLowerCase();
+    from = Number(from.trim());
+    to = Number(to.trim());
+
+    if (isNaN(from) || isNaN(to)) {
+        throw new Error('Invalid search parameters for from..to range');
+    }
+
+    const fieldsEmpty = !(searchText || from || to);
+    // console.log(fieldsEmpty);
+
+    if (fieldsEmpty) return [];
+
+    let results = await getAll();
+
+    if (searchText) {
+        results = results.filter(v => {
+            let name = v.name?.toLowerCase();
+            return name.includes(searchText);
+        });
+    }
+
+    if (from) {
+        results = results.filter(v => {
+            let difficultyLvl = v.difficultyLvl;
+            return difficultyLvl >= from;
+        });
+    }
+
+    if (to) {
+        results = results.filter(v => {
+            let difficultyLvl = v.difficultyLvl;
+            return difficultyLvl <= to;
+        });
+    }
+
+    return results;
+};
+
+const addAccessory = async function (cubeId, accessoryId) {
+    const cube = await Cube.findById(cubeId);
+    const accessory = await Accessory.findById(accessoryId);
+
+    cube.accessories.push(accessory);
+    return cube.save();
+};
+
+module.exports = {
+    get,
+    getAll,
+    create,
+    edit,
+    remove,
+    search,
+    addAccessory
+};
