b97b48309d66b263fb60d7e6e401b2eec746b9a5
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-10-14 18:02:36 +0300

    Updated Accessory model validation

--- src/models/Accessory.js
+++ src/models/Accessory.js
@@ -3,24 +3,31 @@ const mongoose = require('mongoose');
 const accessorySchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'Name is required.']
+        required: [true, 'Name is required.'],
+        minlength: [5, 'Accessory name must be at least 5 characters long.'],
+        validate: {
+            validator: function(v) {
+                return /[a-z0-9 ]/i.test(v);
+            },
+            message: 'Accessory name can consist only of english letters, digits and white spaces.'
+        }
     },
     description: {
         type: String,
         required: [true, 'Description is required.'],
-        maxlength: 500
+        minlength: [20, 'Accessory description must be at least 20 characters long.'],
+        validate: {
+            validator: function(v) {
+                return /[a-z0-9 ]/i.test(v);
+            },
+            message: 'Accessory description can consist only of english letters, digits and white spaces.'
+        }
     },
     imageUrl: {
         type: String,
         required: [true, 'Image URL is required.'],
         validate: [/^https?:\/\//i, 'Inavalid URL format.']
-    },
-    // cubes: [
-    //     {
-    //         type: mongoose.Types.ObjectId,
-    //         ref: 'Cube'
-    //     }
-    // ]
+    }
 });
 
 const Accessory = mongoose.model('Accessory', accessorySchema);
