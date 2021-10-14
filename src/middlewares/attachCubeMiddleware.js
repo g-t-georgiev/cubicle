17c2ba03700aaf5e050a8065afa9ffa8b6d8f870
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-10-14 20:39:44 +0300

    Update attachCubeMiddleware.js

--- src/middlewares/attachCubeMiddleware.js
+++ src/middlewares/attachCubeMiddleware.js
@@ -2,6 +2,7 @@ const attachCubeMiddleware = async function (req, res, next) {
     try {
         const { cubeId } = req.params;
         const cube = await cubeService.get(cubeId);
+        cube.name = cube.name?.[0] + cube.name?.slice(1);
         req.cube = cube;
         return next();
     } catch (error) {
