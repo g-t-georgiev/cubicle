4c54f03ba93a9ec95d7ab70f034a9b062bc7bd57
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-10-14 19:44:48 +0300

    Update cubeController.js

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -5,98 +5,46 @@ const cubeAccessoryController = require('./cubeAccessoryController');
 
 const renderCubeDifficultyOptions = require('../helpers/renderCubeDifficultyOptions');
 
-const isAuthenticated = function (req, res, next) {
-    const { user } = req;
+const attachCubeMiddleware = require('../middlewares/attachCubeMiddleware');
+const userStatusMiddleware = require('../middlewares/userStatusMiddleware');
 
-    if (!user) {
-        return res.redirect('/');
-    }
-
-    next();
-};
-
-const isAuthorized = function (req, res, next) {
-    const { user, params: urlParams } = req;
-
-    const { cubeId } = urlParams;
-    const userId = user?._id;
-
-    return cubeService.get(cubeId)
-        .then(cube => {
-            const { creatorId } = cube;
-
-            if (!creatorId || creatorId !== userId) {
-                return Promise.reject('You do not have permission to modify this resource.');
-            }
-
-            req.cube = cube;
-
-            return next();
-        })
-        .catch(err => {
-            console.log(err.message);
-            return res.redirect(`/cubes/${cubeId}/details`);
-        });
+const filterRequests = function (req, res, next) {
+    const { hasPermissions } = req;
+    return hasPermissions ? next() : res.redirect('/');
 };
 
 let options = null;
 
 const renderCreateCubePageHandler = (req, res) => {
-    const { isAuthenticated, user } = req;
+    const { user } = req;
 
-    options = {
-        isAuthenticated,
-        creatorId: user._id
-    };
+    const creatorId = user._id;
+    options = { creatorId };
 
     res.render('cubes/create', options);
 };
 
 const renderCubeDetailsPageHandler = async function (req, res) {
-    const { isAuthenticated, params: urlParams } = req;
-    
-    const userId = req.user?._id;
-    const { cubeId } = urlParams;
-
-    try {
-        const cube = await cubeService.get(cubeId);
-
-        const { creatorId } = cube;
-
-        options = {
-            ...cube,
-            isAuthenticated,
-            canEdit: isAuthenticated && userId === creatorId
-        };
-
-        res.render('cubes/details', options);
-    } catch(err) {
-        res.status.render('404');
-    }
-};
+    const { cube } = req;
+    options = { ...cube };
+    res.render('cubes/details', options);
+}
 
 const createCubeHandler = async function (req, res) {
-    const { name, description, imageUrl, difficultyLvl, creatorId } = req.body;
+    const { name, description, imageUrl, difficulty, creatorId } = req.body;
 
     try {
-        await cubeService.create(name, description, imageUrl, difficultyLvl, creatorId);
+        await cubeService.create(name, description, imageUrl, difficulty, creatorId);
         res.redirect('/');
-    } catch (err) {
-        res.status(500).send(err.message);
+    } catch (error) {
+        res.status(500).send(error.message);
     }
 };
 
 const renderEditCubePageHandler = async function (req, res) {
-    const { isAuthenticated, params: urlParams, cube } = req;
-
-    const { cubeId } = urlParams;
-
-    options = {
-        ...cube,
-        isAuthenticated,
-        difficultyLevels: renderCubeDifficultyOptions(cube.difficultyLvl)
-    };
-
+    const { cube } = req;
+    const difficulties = renderCubeDifficultyOptions(cube.difficulty);
+    options = { ...cube, difficulties };
     res.render('cubes/edit', options);
 };
 
@@ -107,22 +55,15 @@ const editCubeHandler = async function (req, res) {
     try {
         await cubeService.edit(cubeId, data);
         res.redirect(`/cubes/${cubeId}/details`);
-    } catch (err) {
-        res.status(400).send(err.message);
+    } catch (error) {
+        res.status(400).send(error.message);
     }
 };
 
 const renderDeleteCubePageHandler = async function (req, res) {
-    const { isAuthenticated, params: urlParams, cube } = req;
-
-    const { cubeId } = urlParams;
-
-    options = {
-        ...cube,
-        isAuthenticated,
-        difficultyLevels: renderCubeDifficultyOptions(cube.difficultyLvl)
-    };
-
+    const { cube } = req;
+    const difficulties = renderCubeDifficultyOptions(cube.difficulty);
+    options = { ...cube, difficulties };
     res.render('cubes/delete', options);
 };
 
@@ -132,18 +73,21 @@ const deleteCubeHandler = async function (req, res) {
     try {
         await cubeService.remove(cubeId);
         res.redirect('/');
-    } catch (err) {
-        res.status(400).send(err.message);
+    } catch (error) {
+        res.status(400).send(error.message);
     }
 };
 
+router.use(attachCubeMiddleware);
+router.use(userStatusMiddleware);
+
 router.get('/:cubeId/details', renderCubeDetailsPageHandler);
-router.get('/:cubeId/edit', isAuthenticated, isAuthorized, renderEditCubePageHandler);
-router.post('/:cubeId/edit', isAuthenticated, isAuthorized, editCubeHandler);
-router.get('/:cubeId/delete', isAuthenticated, isAuthorized, renderDeleteCubePageHandler);
-router.post('/:cubeId/delete', isAuthenticated, isAuthorized, deleteCubeHandler);
-router.use('/:cubeId/accessories', isAuthenticated, isAuthorized, cubeAccessoryController);
-router.get('/create', isAuthenticated, renderCreateCubePageHandler);
-router.post('/create', isAuthenticated, createCubeHandler);
+router.get('/:cubeId/edit', filterRequests, renderEditCubePageHandler);
+router.post('/:cubeId/edit', filterRequests, editCubeHandler);
+router.get('/:cubeId/delete', filterRequests, renderDeleteCubePageHandler);
+router.post('/:cubeId/delete', filterRequests, deleteCubeHandler);
+router.use('/:cubeId/accessories', filterRequests, cubeAccessoryController);
+router.get('/create', filterRequests, renderCreateCubePageHandler);
+router.post('/create', filterRequests, createCubeHandler);
 
 module.exports = router;
