a6a5e787382e4b1aae564e31c132057d89390ba9
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-10-28 20:37:57 +0300

    Added routes module inside express config file.

--- src/config/express.js
+++ src/config/express.js
@@ -2,23 +2,16 @@ const express = require('express');
 const path = require('path');
 const { urlencoded } = express;
 const cookieParser = require('cookie-parser');
+const routes = require('./routes');
 
 const handlebars = require('./handlebars');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 module.exports = (app) => {
-    // Setup cookie parser
     app.use(cookieParser());
-
-    // Setup user authentication
     authMiddleware(app);
-
-    // Setup handlebars
     handlebars(app);
-
-    // Setup static files
     app.use('/static', express.static(path.join(__dirname, '../public')));
-
-    // Setup form request body parsing
     app.use(urlencoded({ extended: true }));
+    app.use(routes);
 };
