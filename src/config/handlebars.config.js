577ae931717281efcd82ba43b069767bf5bba8f9
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-10-28 21:20:21 +0300

    Deleted render cube difficulty options from helpers folder and added it as handlebars helper.

--- src/config/handlebars.config.js
+++ src/config/handlebars.config.js
@@ -1,5 +1,25 @@
 const exphbs = require('express-handlebars');
-const handlebars = exphbs.create({ extname: 'hbs' });
+const handlebars = exphbs.create({
+    extname: 'hbs',
+    helpers: {
+        *options(difficulty) {
+            // console.log(difficulty);
+
+            const text = {
+                1: '1 - Very Easy',
+                2: '2 - Easy',
+                3: '3 - Medium (Standard 3x3)',
+                4: '4 - Intermediate',
+                5: '5 - Expert',
+                6: '6 - Hardcore'
+            };
+
+            for (let index = 1; index <= 6; index++) {
+                yield `<option value="${index}" ${difficulty === index ? 'selected' : ''}>${text[index]}</option>`;
+            }
+        }
+    }
+});
 const path = require('path');
 
 module.exports = (app) => {
